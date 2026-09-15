0dac442225fda16c9d90cac01e35cc75b107904a
diliab <dberrios3975@sdsu.edu>
2026-09-15 11:12:44 -0500

    Initial commit

---
+++ data.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dilia Berrios on 9/15/26.
+//
+
+#include "data.h"
---
+++ data.h
@@ -0,0 +1,7 @@
+#ifndef MACHINELEARNING_DATA_H
+#define MACHINELEARNING_DATA_H
+#include <sstream>
+
+
+std::vector<double> temperatures = {72.0, 85.0, 95.0, 60.0, 102.0};
+std::vector<double> tree_health  = {90.0, 75.0, 50.0, 95.0,  30.0};
